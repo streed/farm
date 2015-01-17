dc38ac138328d866608b027ce3b419ce7a86b95b
Sean Reed <sean@cheapfit.me>
2015-01-17 02:08:03 -0500

    fix: Fix guice version.

--- sensor-backend/SensorService/src/main/java/com/farm/sensor/service/SensorServiceModule.java
+++ sensor-backend/SensorService/src/main/java/com/farm/sensor/service/SensorServiceModule.java
@@ -2,11 +2,8 @@ package com.farm.sensor.service;
 
 import com.google.inject.AbstractModule;
 import com.google.inject.Provides;
-import com.google.inject.Singleton;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.hbase.HBaseConfiguration;
-import redis.clients.jedis.JedisPool;
-import redis.clients.jedis.JedisPoolConfig;
 import com.farm.sensor.data.SensorDataModule;
 
 public class SensorServiceModule extends AbstractModule {
